91f47181021fac293ab5530591b3190a6203aeb1
Varun Chawla <26588809+TheVarunChawla@users.noreply.github.com>
2026-06-08 15:03:58 +0530

    Update fetch_news.py

--- fetch_news.py
+++ fetch_news.py
@@ -32,35 +32,57 @@ feeds = {
         "https://msrc.microsoft.com/blog/feed",
 
     "Google Security Blog":
-        "https://security.googleblog.com/feeds/posts/default"
+        "https://security.googleblog.com/feeds/posts/default",
 
+    "Microsoft Security Blog":
+        "https://www.microsoft.com/en-us/security/blog/feed/"
 }
 
-output = "# Daily Cyber News\n\n"
+skip_words = [
+    "sponsored",
+    "webinar",
+    "podcast",
+    "advertisement"
+]
+
+output = "# Daily Cyber Intelligence\n\n"
 
 for source, url in feeds.items():
 
+    output += "=" * 60 + "\n"
+    output += f"SOURCE : {source}\n"
+    output += "=" * 60 + "\n\n"
+
     feed = feedparser.parse(url)
 
-    output += f"## {source}\n\n"
+    for article in feed.entries[:3]:
 
-    for article in feed.entries[:5]:
+        title = article.title.lower()
 
-        output += f"### {article.title}\n\n"
+        if any(word in title for word in skip_words):
+            continue
 
-if hasattr(article, "published"):
-    output += f"Published: {article.published}\n\n"
+        output += f"Title:\n{article.title}\n\n"
 
-if hasattr(article, "summary"):
-    summary = article.summary.replace("<p>", "").replace("</p>", "")
-    summary = summary.replace("<br />", "")
-    output += f"Summary:\n{summary}\n\n"
+        if hasattr(article, "published"):
+            output += f"Published:\n{article.published}\n\n"
 
-output += f"URL:\n{article.link}\n\n"
+        if hasattr(article, "summary"):
 
-output += "--------------------------------------------\n\n"
+            summary = article.summary
+
+            summary = summary.replace("<p>", "")
+            summary = summary.replace("</p>", "")
+            summary = summary.replace("<br />", "")
+            summary = summary.replace("&nbsp;", " ")
+
+            output += f"Summary:\n{summary}\n\n"
+
+        output += f"URL:\n{article.link}\n\n"
+
+        output += "-" * 60 + "\n\n"
 
 with open("daily_news.md", "w", encoding="utf-8") as f:
     f.write(output)
 
-print("News file generated successfully!")
+print("Daily Cyber Intelligence file generated successfully!")
